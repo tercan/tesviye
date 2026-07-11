d234158c7e36aed63b1381a3a818e596b8e9c2b0
Tercan Keskin <tercankeskin@gmail.com>
2026-07-11 17:44:54 +0300

    Initial Commit

---
+++ tesviye/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tesviye
+//
+//  Created by Tercan Keskin on 11.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
